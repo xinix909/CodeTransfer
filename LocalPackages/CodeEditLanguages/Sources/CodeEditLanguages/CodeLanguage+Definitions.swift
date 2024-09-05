6ccffc15d717728b7e9c6e0e2163cf9c077b8129
xinix909 <386887912@qq.com>
2024-09-06 02:47:17 +0800

    add CodeEditLanguages

---
+++ LocalPackages/CodeEditLanguages/Sources/CodeEditLanguages/CodeLanguage+Definitions.swift
@@ -0,0 +1,468 @@
+// swiftlint:disable all
+
+//
+//  CodeLanguage+Definitions.swift
+//  
+//
+//  Created by Lukas Pistrol on 15.01.23.
+//
+
+import Foundation
+
+public extension CodeLanguage {
+
+    /// An array of all language structures.
+    static let allLanguages: [CodeLanguage] = [
+        .agda,
+        .bash,
+        .c,
+        .cpp,
+        .cSharp,
+        .css,
+        .dart,
+        .dockerfile,
+        .elixir,
+        .go,
+        .goMod,
+        .haskell,
+        .html,
+        .java,
+        .javascript,
+        .jsdoc,
+        .json,
+        .jsx,
+        .julia,
+        .kotlin,
+        .lua,
+        .markdown,
+        .markdownInline,
+        .objc,
+        .ocaml,
+        .ocamlInterface,
+        .perl,
+        .php,
+        .python,
+        .regex,
+        .ruby,
+        .rust,
+        .scala,
+        .sql,
+        .swift,
+        .toml,
+        .tsx,
+        .typescript,
+        .verilog,
+        .yaml,
+        .zig
+    ]
+
+    /// A language structure for `Agda`
+    static let agda: CodeLanguage = .init(
+        id: .agda,
+        tsName: "agda",
+        extensions: ["agda"],
+        lineCommentString: "--",
+        rangeCommentStrings: ("{-", "-}")
+    )
+
+    /// A language structure for `Bash`
+    static let bash: CodeLanguage = .init(
+        id: .bash,
+        tsName: "bash",
+        extensions: ["sh", "bash"],
+        lineCommentString: "#",
+        rangeCommentStrings: (":'", "'")
+    )
+
+    /// A language structure for `C`
+    static let c: CodeLanguage = .init(
+        id: .c,
+        tsName: "c",
+        extensions: ["c", "h"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `C++`
+    static let cpp: CodeLanguage = .init(
+        id: .cpp,
+        tsName: "cpp",
+        extensions: ["cc", "cpp", "c++", "hpp", "h"],
+        lineCommentString: CodeLanguage.c.lineCommentString,
+        rangeCommentStrings: CodeLanguage.c.rangeCommentStrings,
+        documentationCommentStrings: [.pair(("/**", "*/"))],
+        parentURL: CodeLanguage.c.queryURL,
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `C#`
+    static let cSharp: CodeLanguage = .init(
+        id: .cSharp,
+        tsName: "c-sharp",
+        extensions: ["cs"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `CSS`
+    static let css: CodeLanguage = .init(
+        id: .css,
+        tsName: "css",
+        extensions: ["css"],
+        lineCommentString: "",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `Dart`
+    static let dart: CodeLanguage = .init(
+        id: .dart,
+        tsName: "dart",
+        extensions: ["dart"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `Dockerfile`
+    static let dockerfile: CodeLanguage = .init(
+        id: .dockerfile,
+        tsName: "dockerfile",
+        extensions: ["Dockerfile"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("", "")
+    )
+
+    /// A language structure for `Elixir`
+    static let elixir: CodeLanguage = .init(
+        id: .elixir,
+        tsName: "elixir",
+        extensions: ["ex", "exs"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("", ""),
+        documentationCommentStrings: [.pair(("\"\"\"", "\"\"\""))],
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Go`
+    static let go: CodeLanguage = .init(
+        id: .go,
+        tsName: "go",
+        extensions: ["go"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `GoMod`
+    static let goMod: CodeLanguage = .init(
+        id: .goMod,
+        tsName: "go-mod",
+        extensions: ["mod"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `Haskell`
+    static let haskell: CodeLanguage = .init(
+        id: .haskell,
+        tsName: "haskell",
+        extensions: ["hs"],
+        lineCommentString: "--",
+        rangeCommentStrings: ("{-", "-}")
+    )
+
+    /// A language structure for `HTML`
+    static let html: CodeLanguage = .init(
+        id: .html,
+        tsName: "html",
+        extensions: ["html", "htm", "shtml"],
+        lineCommentString: "",
+        rangeCommentStrings: ("<!--", "-->"),
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Java`
+    static let java: CodeLanguage = .init(
+        id: .java,
+        tsName: "java",
+        extensions: ["java", "jav"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        documentationCommentStrings: [.pair(("/**", "*/"))]
+    )
+
+    /// A language structure for `JavaScript`
+    static let javascript: CodeLanguage = .init(
+        id: .javascript,
+        tsName: "javascript",
+        extensions: ["js", "cjs", "mjs"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        documentationCommentStrings: [.pair(("/**", "*/"))],
+        highlights: ["injections"],
+        additionalIdentifiers: ["node", "deno"]
+    )
+
+    /// A language structure for `JSDoc`
+    static let jsdoc: CodeLanguage = .init(
+        id: .jsdoc,
+        tsName: "jsdoc",
+        extensions: [],
+        lineCommentString: "",
+        rangeCommentStrings: ("/**", "*/")
+    )
+
+    /// A language structure for `JSON`
+    static let json: CodeLanguage = .init(
+        id: .json,
+        tsName: "json",
+        extensions: ["json"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `JSX`
+    static let jsx: CodeLanguage = .init(
+        id: .jsx,
+        tsName: "javascript",
+        extensions: ["jsx"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        highlights: ["highlights-jsx", "injections"]
+    )
+
+    /// A language structure for `Julia`
+    static let julia: CodeLanguage = .init(
+        id: .julia,
+        tsName: "julia",
+        extensions: ["jl"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("#=", "=#")
+    )
+
+    /// A language structure for `Kotlin`
+    static let kotlin: CodeLanguage = .init(
+        id: .kotlin,
+        tsName: "kotlin",
+        extensions: ["kt", "kts"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `Lua`
+    static let lua: CodeLanguage = .init(
+        id: .lua,
+        tsName: "lua",
+        extensions: ["lua"],
+        lineCommentString: "--",
+        rangeCommentStrings: ("-[[", "]]--"),
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Markdown`
+    static let markdown: CodeLanguage = .init(
+        id: .markdown,
+        tsName: "markdown",
+        extensions: ["md", "mkd", "mkdn", "mdwn", "mdown", "markdown"],
+        lineCommentString: "[comment]: #",
+        rangeCommentStrings: ("", ""),
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Markdown Inline`
+    static let markdownInline: CodeLanguage = .init(
+        id: .markdownInline,
+        tsName: "markdown-inline",
+        extensions: [],
+        lineCommentString: "",
+        rangeCommentStrings: ("", ""),
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Objective C`
+    static let objc: CodeLanguage = .init(
+        id: .objc,
+        tsName: "objc",
+        extensions: ["m", "h"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `OCaml`
+    static let ocaml: CodeLanguage = .init(
+        id: .ocaml,
+        tsName: "ocaml",
+        extensions: ["ml"],
+        lineCommentString: "",
+        rangeCommentStrings: ("(*", "*)"),
+        documentationCommentStrings: [.pair(("(**", "*)"))]
+    )
+
+    /// A language structure for `OCaml Interface`
+    static let ocamlInterface: CodeLanguage = .init(
+        id: .ocamlInterface,
+        tsName: "ocaml",
+        extensions: ["mli"],
+        lineCommentString: "",
+        rangeCommentStrings: ("", "")
+    )
+
+    /// A language structure for `Perl`
+    static let perl: CodeLanguage = .init(
+        id: .perl,
+        tsName: "perl",
+        extensions: ["pl", "pm"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("=pod", "=cut")
+    )
+
+    /// A language structure for `PHP`
+    static let php: CodeLanguage = .init(
+        id: .php,
+        tsName: "php",
+        extensions: ["php"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Python`
+    static let python: CodeLanguage = .init(
+        id: .python,
+        tsName: "python",
+        extensions: ["py"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("", ""),
+        documentationCommentStrings: [.pair(("\"\"\"", "\"\"\""))],
+        additionalIdentifiers: ["python2", "python3"]
+    )
+
+    /// A language structure for `Regex`
+    static let regex: CodeLanguage = .init(
+        id: .regex,
+        tsName: "regex",
+        extensions: [],
+        lineCommentString: "",
+        rangeCommentStrings: ("", "")
+    )
+
+    /// A language structure for `Ruby`
+    static let ruby: CodeLanguage = .init(
+        id: .ruby,
+        tsName: "ruby",
+        extensions: ["rb"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("=begin", "=end")
+    )
+
+    /// A language structure for `Rust`
+    static let rust: CodeLanguage = .init(
+        id: .rust,
+        tsName: "rust",
+        extensions: ["rs"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        documentationCommentStrings: [
+            .single("///"),
+            .single("//!"),
+            .pair(("/**", "*/")),
+            .pair(("/*!", "*/"))
+        ],
+        highlights: ["injections"]
+    )
+
+    /// A language structure for `Scala`
+    static let scala: CodeLanguage = .init(
+        id: .scala,
+        tsName: "scala",
+        extensions: ["scala", "sc"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `SQL`
+    static let sql: CodeLanguage = .init(
+        id: .sql,
+        tsName: "sql",
+        extensions: ["sql"],
+        lineCommentString: "--",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `Swift`
+    static let swift: CodeLanguage = .init(
+        id: .swift,
+        tsName: "swift",
+        extensions: ["swift"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        documentationCommentStrings: [.single("///"), .pair(("/**", "*/"))]
+    )
+
+    /// A language structure for `TOML`
+    static let toml: CodeLanguage = .init(
+        id: .toml,
+        tsName: "toml",
+        extensions: ["toml"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("", "")
+    )
+
+    /// A language structure for `TSX`
+    static let tsx: CodeLanguage = .init(
+        id: .tsx,
+        tsName: "typescript",
+        extensions: ["tsx"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        parentURL: CodeLanguage.jsx.queryURL
+    )
+
+    /// A language structure for `Typescript`
+    static let typescript: CodeLanguage = .init(
+        id: .typescript,
+        tsName: "typescript",
+        extensions: ["ts", "cts", "mts"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/"),
+        parentURL: CodeLanguage.javascript.queryURL
+    )
+
+    /// A language structure for `Verilog`
+    static let verilog: CodeLanguage = .init(
+        id: .verilog,
+        tsName: "verilog",
+        extensions: ["v"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("/*", "*/")
+    )
+
+    /// A language structure for `YAML`
+    static let yaml: CodeLanguage = .init(
+        id: .yaml,
+        tsName: "yaml",
+        extensions: ["yml", "yaml"],
+        lineCommentString: "#",
+        rangeCommentStrings: ("", "")
+    )
+
+    /// A language structure for `Zig`
+    static let zig: CodeLanguage = .init(
+        id: .zig,
+        tsName: "zig",
+        extensions: ["zig"],
+        lineCommentString: "//",
+        rangeCommentStrings: ("", ""),
+        documentationCommentStrings: [.single("///"), .single("//!")],
+        highlights: ["injections"]
+    )
+
+    /// The default language (plain text)
+    static let `default`: CodeLanguage = .init(
+        id: .plainText,
+        tsName: "PlainText",
+        extensions: ["txt"],
+        lineCommentString: "",
+        rangeCommentStrings: ("", "")
+    )
+}
+
+// swiftlint:enable file_length
---
+++ LocalPackages/CodeEditLanguages/Sources/CodeEditLanguages/CodeLanguage.swift
@@ -0,0 +1,220 @@
+//
+//  CodeLanguage.swift
+//  CodeEditTextView/CodeLanguage
+//
+//  Created by Lukas Pistrol on 25.05.22.
+//
+
+import Foundation
+import tree_sitter
+import SwiftTreeSitter
+import CodeLanguages_Container
+import RegexBuilder
+
+/// A structure holding metadata for code languages
+public struct CodeLanguage {
+    internal init(
+        id: TreeSitterLanguage,
+        tsName: String,
+        extensions: Set<String>,
+        lineCommentString: String,
+        rangeCommentStrings: (String, String),
+        documentationCommentStrings: Set<DocumentationComments> = [],
+        parentURL: URL? = nil,
+        highlights: Set<String>? = nil,
+        additionalIdentifiers: Set<String> = []
+    ) {
+        self.id = id
+        self.tsName = tsName
+        self.extensions = extensions
+        self.lineCommentString = lineCommentString
+        self.rangeCommentStrings = rangeCommentStrings
+        self.documentationCommentStrings = documentationCommentStrings
+        self.parentQueryURL = parentURL
+        self.additionalHighlights = highlights
+        self.additionalIdentifiers = additionalIdentifiers
+    }
+
+    /// The ID of the language
+    public let id: TreeSitterLanguage
+
+    /// The display name of the language
+    public let tsName: String
+
+    /// A set of file extensions for the language
+    ///
+    /// In special cases this can also be a file name
+    /// (e.g `Dockerfile`, `Makefile`)
+    public let extensions: Set<String>
+
+    /// The leading string of a comment line
+    public let lineCommentString: String
+
+    /// The leading and trailing string of a multi-line comment
+    public let rangeCommentStrings: (String, String)
+
+    /// The leading (and trailing, if there is one) string of a documentation comment
+    public let documentationCommentStrings: Set<DocumentationComments>
+
+    /// The query URL of a language this language inherits from. (e.g.: C for C++)
+    public let parentQueryURL: URL?
+
+    /// Additional highlight file names (e.g.: JSX for JavaScript)
+    public let additionalHighlights: Set<String>?
+
+    /// The query URL for the language if available
+    public var queryURL: URL? {
+        queryURL()
+    }
+
+    /// The bundle's resource URL
+    internal var resourceURL: URL? = Bundle.module.resourceURL
+
+    /// A set of aditional identifiers to use for things like shebang matching.
+    public let additionalIdentifiers: Set<String>
+
+    /// The tree-sitter language for the language if available
+    public var language: Language? {
+        guard let tsLanguage = tsLanguage else { return nil }
+        return Language(language: tsLanguage)
+    }
+
+    internal func queryURL(for highlights: String = "highlights") -> URL? {
+        return resourceURL?
+            .appendingPathComponent("Resources/tree-sitter-\(tsName)/\(highlights).scm")
+    }
+
+    /// Gets the TSLanguage from `tree-sitter`
+    private var tsLanguage: UnsafeMutablePointer<TSLanguage>? {
+        switch id {
+        case .agda:
+            return tree_sitter_agda()
+        case .bash:
+            return tree_sitter_bash()
+        case .c:
+            return tree_sitter_c()
+        case .cpp:
+            return tree_sitter_cpp()
+        case .cSharp:
+            return tree_sitter_c_sharp()
+        case .css:
+            return tree_sitter_css()
+        case .dart:
+            return tree_sitter_dart()
+        case .dockerfile:
+            return tree_sitter_dockerfile()
+        case .elixir:
+            return tree_sitter_elixir()
+        case .go:
+            return tree_sitter_go()
+        case .goMod:
+            return tree_sitter_gomod()
+        case .haskell:
+            return tree_sitter_haskell()
+        case .html:
+            return tree_sitter_html()
+        case .java:
+            return tree_sitter_java()
+        case .javascript:
+            return tree_sitter_javascript()
+        case .jsdoc:
+            return tree_sitter_jsdoc()
+        case .json:
+            return tree_sitter_json()
+        case .jsx:
+            return tree_sitter_javascript()
+        case .julia:
+            return tree_sitter_julia()
+        case .kotlin:
+            return tree_sitter_kotlin()
+        case .lua:
+            return tree_sitter_lua()
+        case .markdown:
+            return tree_sitter_markdown()
+        case .markdownInline:
+            return tree_sitter_markdown_inline()
+        case .objc:
+            return tree_sitter_objc()
+        case .ocaml:
+            return tree_sitter_ocaml()
+        case .ocamlInterface:
+            return tree_sitter_ocaml_interface()
+        case .perl:
+            return tree_sitter_perl()
+        case .php:
+            return tree_sitter_php()
+        case .python:
+            return tree_sitter_python()
+        case .regex:
+            return tree_sitter_regex()
+        case .ruby:
+            return tree_sitter_ruby()
+        case .rust:
+            return tree_sitter_rust()
+        case .scala:
+            return tree_sitter_scala()
+        case .sql:
+            return tree_sitter_sql()
+        case .swift:
+            return tree_sitter_swift()
+        case .toml:
+            return tree_sitter_toml()
+        case .tsx:
+            return tree_sitter_tsx()
+        case .typescript:
+            return tree_sitter_typescript()
+        case .verilog:
+            return tree_sitter_verilog()
+        case .yaml:
+            return tree_sitter_yaml()
+        case .zig:
+            return tree_sitter_zig()
+        case .plainText:
+            return nil
+        }
+    }
+}
+
+extension CodeLanguage: Hashable {
+    public static func == (lhs: CodeLanguage, rhs: CodeLanguage) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+public enum DocumentationComments: Hashable {
+    public static func == (lhs: DocumentationComments, rhs: DocumentationComments) -> Bool {
+        switch lhs {
+        case .single(let lhsString):
+            switch rhs {
+            case .single(let rhsString):
+                return lhsString == rhsString
+            case .pair:
+                return false
+            }
+        case .pair(let lhsPair):
+            switch rhs {
+            case .single:
+                return false
+            case .pair(let rhsPair):
+                return lhsPair.0 == rhsPair.0 && lhsPair.1 == rhsPair.1
+            }
+        }
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+        case .single(let string):
+            hasher.combine(string)
+        case .pair(let pair):
+            hasher.combine(pair.0)
+            hasher.combine(pair.1)
+        }
+    }
+
+    case single(String)
+    case pair((String, String))
+}
